20d77717247589fc516f3f5d9ab2e802f81bc2d2
ibobylev1997 <86730222+ibobylev1997@users.noreply.github.com>
2023-03-16 02:08:12 +0300

    Add files via upload

---
+++ task.py
@@ -0,0 +1,48 @@
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.models.baseoperator import BaseOperator
+from airflow.utils.decorators import apply_defaults
+import requests
+
+class UrltoPostgresOperator(BaseOperator):
+    
+    @apply_defaults
+    def __init__(self,
+                url,
+                target_schema,
+                target_table,
+                *args,
+                **kwargs
+                ):
+        super().__init__(*args, **kwargs)
+        self.url = url
+        self.target_schema = target_schema
+        self.target_table = target_table
+
+    def get_data_from_api():
+
+        response = requests.get(self.url)
+
+        if response.status_code == 200:
+            data = response.json()
+            for item in data: #creating generator
+                yield item
+        else:
+            print('Error: ' + str(response.status_code))
+        
+    def load_data_to_postgres():
+
+        source = PostgresHook(postgres_conn_id='my_postgres_connection')
+
+        conn = source.get_conn()
+        cursor = conn.cursor()
+
+        for item in get_data_from_api():
+            query = "INSERT INTO {}.{} {} VALUES {}".format(self.target_schema, \
+                                                            self.target_table,[i for i in item.keys()], \
+                                                            [item[i] for i in item.keys()]) #creating query for inserting
+            query = query.replace("[", "(").replace("]", ")") 
+
+            cursor.execute(query)
+
+        cursor.close()
+        conn.close()
